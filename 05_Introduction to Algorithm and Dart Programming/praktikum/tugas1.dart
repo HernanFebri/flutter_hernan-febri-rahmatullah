ab79a88cff42c27e9390e2ab639b72d2cd5ffe86
Hernan Febri <hernanfebri@gmail.com>
2022-08-30 11:59:09 +0700

    Revisi Praktikum Section 5

--- 05_Introduction to Algorithm and Dart Programming/praktikum/tugas1.dart
+++ 05_Introduction to Algorithm and Dart Programming/praktikum/tugas1.dart
@@ -3,6 +3,6 @@ import 'dart:math';
 void main() {
   pi;
   int r = 10;
-  double luaslingkaran = pi*r;
-  print("luaslingkaran = $pi x $r = $luaslingkaran"); 
+  double luaslingkaran = pi*r*r;
+  print("luaslingkaran = $pi x $r x $r = $luaslingkaran"); 
 }
