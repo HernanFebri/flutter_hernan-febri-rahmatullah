6032cc7ed0ea14fbdb6d98b565cca4cde40ce819
Hernan Febri <hernanfebri@gmail.com>
2022-08-30 12:58:00 +0700

    Praktikum Section 6

---
+++ 06_Basic Dart Programming/praktikum/tugas1.dart
@@ -0,0 +1,8 @@
+import 'dart:math';
+
+void main() {
+  pi;
+  int r = 10;
+  double luaslingkaran = pi*r*r;
+  print("luaslingkaran = $pi x $r x $r = $luaslingkaran"); 
+}
---
+++ 06_Basic Dart Programming/praktikum/tugas2.dart
@@ -0,0 +1,11 @@
+void main() {
+  String nama;
+  String asalKota;
+  String asalUniversitas;
+
+  nama = "Hernan Febri Rahmatullah";
+  asalKota = "Purwokerto";
+  asalUniversitas = "Universitas Amikom Purwokerto";
+
+  print("Halo, nama saya${nama}, saya berasal dari ${asalKota}. Saat ini saya sedang berkuliah di ${asalUniversitas}");
+}
