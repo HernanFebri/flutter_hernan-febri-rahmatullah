0fc55de3c3ded621a0415ba050c369ea343206bc
Hernan Febri <hernanfebri@gmail.com>
2022-09-15 16:00:36 +0700

    Praktikum 12

---
+++ 12_Introduction Flutter Widget/praktikum/flutter_application_1/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'taks1.dart';
+import 'taks2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ 12_Introduction Flutter Widget/praktikum/flutter_application_1/lib/taks1.dart
@@ -0,0 +1,58 @@
+// import 'package:flutter/material.dart';
+// import 'package:slide_digital_clock/slide_digital_clock.dart';
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Widget'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   void _incrementCounter() {
+//     setState(() {});
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             DigitalClock(
+//               digitAnimationStyle: Curves.elasticInOut,
+//               is24HourTimeFormat: false,
+//               areaDecoration: BoxDecoration(
+//                 color: Colors.transparent,
+//               ),
+//               hourMinuteDigitTextStyle:
+//                   TextStyle(color: Colors.black, fontSize: 50),
+//               amPmDigitTextStyle:
+//                   TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
